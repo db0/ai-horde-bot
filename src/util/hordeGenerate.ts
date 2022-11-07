9db6aa26d9398192d8c7d6a2dce3ebb4996e3599
ZeldaFan0225 <robin.butsch@gmail.com>
2022-11-07 16:54:35 +0100

    Revert "reject promise if generation faulted"
    This reverts commit 7ee4d64c0d273975fb656ca6df52c3612a9687df.

--- src/util/hordeGenerate.ts
+++ src/util/hordeGenerate.ts
@@ -32,11 +32,6 @@ export default async function (
                         return;
                     }
 
-                    if(checkResult.faulted) {
-                        reject("Generation faulted");
-                        return;
-                    }
-
                     if (checkResult.done) {
                         clearInterval(checkInterval);
                         API.getAsyncStatus(data.id)
